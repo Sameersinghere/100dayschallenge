7c349e5e48d22426452637404fa187287fa0ccf9
Singh.sameer <66819024+Sameersinghere@users.noreply.github.com>
2023-11-30 17:36:45 +0530

    Create ripple_carry_counter.v
    This code shows how I have instantiated a T flipflop in a ripple carry counter.
    Instantiation is used to reduce code lines as it uses another code block in a bigger code.

---
+++ Day3/ripple_carry_counter.v
@@ -0,0 +1,18 @@
+module ripple_carry_counter(q,clk,reset);
+  output [3:0] q;
+  input clk,reset;
+
+  T_FF tff0(q[0],clk,reset);
+  T_FF tff1(q[1],q[0],reset);
+  T_FF tff2(q[2],q[1],reset);
+  T_FF tff3(q[3],q[2],reset);
+endmodule
+
+// Flipflop T_FF
+module T_FF(q,clk,reset);
+  output q;
+  input clk, reset;
+  wire d;
+  D_FF dff0(q,d,clk,reset);
+  not n1(d,q);
+endmodule
