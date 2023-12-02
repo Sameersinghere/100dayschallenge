38c8730c221dace358b09b43702cef29ba908678
Singh.sameer <66819024+Sameersinghere@users.noreply.github.com>
2023-12-02 17:21:38 +0530

    Create multiplier_tb.v
    This code contains the test bench of the multiplier code

---
+++ Day5/multiplier_tb.v
@@ -0,0 +1,19 @@
+module multiplier_tb;
+	reg [3:0]a,b;
+	wire [7:0]out;
+
+	//wire[7:0]t1,t2,t3,t4;
+
+Multiplier uut0(.a(a),.b(b),.out(out));
+		initial begin
+			a=4'b0;
+			b=4'b0;
+			#100;
+		end
+
+		always begin
+			#100;
+			a=$random;
+			b=$random;
+		end
+endmodule
